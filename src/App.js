e51adc246eb47393e422459ed238c7540c500020
Facudno Petre <facup3@gmail.com>
2021-03-24 12:30:35 -0300

    Simple testing demo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+
+const useCounter = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+  return { count, increment, decrement };
+};
+
+export const Form = ({ handleSubmit }) => {
+  const submitHandler = (ev) => {
+    ev.preventDefault();
+    handleSubmit({ user, password });
+  };
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <form onSubmit={submitHandler}>
+      <label>
+        User
+        <input
+          name="user"
+          value={user}
+          onChange={({ target: { value } }) => setUser(value)}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={({ target: { value } }) => setPassword(value)}
+        />
+      </label>
+      <button>Send</button>
+    </form>
+  );
+};
 
 function App() {
+  const { count, increment, decrement } = useCounter();
+  const [{ name, species, status }, setRick] = useState({});
+  const [isRickLoading, setIsRickLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const getRick = () => {
+    setIsRickLoading(true);
+    setError(null);
+    axios
+      .get(`https://rickandmortyapi.com/api/character/1`)
+      .then(({ data }) => {
+        setRick(data);
+      })
+      .catch((err) => {
+        const data = err?.response?.data;
+        setError(data);
+      })
+      .finally(() => {
+        setIsRickLoading(false);
+      });
+  };
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +77,26 @@ function App() {
         >
           Learn React
         </a>
+        <div>Counter: {count}</div>
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>decrement</button>
+        <button onClick={getRick}>Get Rick</button>
+        <Form handleSubmit={handleSubmit} />
+        {isRickLoading ? (
+          <p>loading</p>
+        ) : (
+          <div>
+            {error ? (
+              <p role="alert">{error.message}</p>
+            ) : (
+              <>
+                <p>name: {name}</p>
+                <p>status: {status}</p>
+                <p>species: {species}</p>
+              </>
+            )}
+          </div>
+        )}
       </header>
     </div>
   );
